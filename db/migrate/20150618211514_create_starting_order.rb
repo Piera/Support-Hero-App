37b4cfd7ac2888ffd66a8d6fcca4799ab9b0beb5
Piera <pierad@gmail.com>
2015-06-18 14:17:45 -0700

    Added starting order model to migration file

--- db/migrate/20150618211514_create_starting_order.rb
+++ db/migrate/20150618211514_create_starting_order.rb
@@ -1,4 +1,13 @@
-class CreateStartingOrder < ActiveRecord::Migration
-  def change
-  end
+class CreateStarting_order < ActiveRecord::Migration
+ def self.up
+   create_table :starting_orders do |t|
+   	 t.belongs_to :hero, index: true
+     t.integer :order
+     t.integer :heroes_id
+   end
+ end
+
+ def self.down
+   drop_table :startingOrders
+ end
 end
