fd8daa96a90d7675b1c40742e45256abc0abd547
Piera <pierad@gmail.com>
2015-06-18 14:02:11 -0700

    Changed migrate file name to accomodate singular model name

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150517230549) do
+
+  create_table "heroes", force: :cascade do |t|
+    t.string "name"
+  end
 
 end
